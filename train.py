08571218d5ebe6e08734eaededa4ffc78df68b9e
Jay Park <benpak2015@gmail.com>
2018-07-30 14:12:25 +0900

    train module

---
+++ train.py
@@ -0,0 +1,134 @@
+from __future__ import print_function, division
+import argparse
+import os
+from os.path import exists, join, basename
+import tensorflow as tf
+#from torch.utils.data import Dataset, DataLoader
+from model.cnn_geometric_model_complete import CNNGeometric
+from model.loss_complete import TransformedGridLoss
+from data.synth_dataset_complete import SynthDataset
+from data.download_datasets_complete import download_pascal
+from geotnf.transformation_complete import SynthPairTnf
+from image.normalization_complete import NormalizeImageDict
+from util.train_test_fn import train, test
+from util.tf_util_complete import save_checkpoint, str_to_bool
+
+"""
+
+Script to train the model as presented in the CNNGeometric CVPR'17 paper
+using synthetically warped image pairs and strong supervision
+
+"""
+
+print('CNNGeometric training script')
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='CNNGeometric TensorFlow implementation')
+# Paths
+parser.add_argument('--training-dataset', type=str, default='pascal', help='dataset to use for training')
+parser.add_argument('--training-tnf-csv', type=str, default='', help='path to training transformation csv folder')
+parser.add_argument('--training-image-path', type=str, default='', help='path to folder containing training images')
+parser.add_argument('--trained-models-dir', type=str, default='trained_models', help='path to trained models folder')
+parser.add_argument('--trained-models-fn', type=str, default='checkpoint_adam', help='trained model filename')
+# Optimization parameters
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum constant')
+parser.add_argument('--num-epochs', type=int, default=10, help='number of training epochs')
+parser.add_argument('--batch-size', type=int, default=16, help='training batch size')
+parser.add_argument('--weight-decay', type=float, default=0, help='weight decay constant')
+parser.add_argument('--seed', type=int, default=1, help='Pseudo-RNG seed')
+# Model parameters
+parser.add_argument('--geometric-model', type=str, default='affine',
+                    help='geometric model to be regressed at output: affine or tps')
+parser.add_argument('--use-mse-loss', type=str_to_bool, nargs='?', const=True, default=False,
+                    help='Use MSE loss on tnf. parameters')
+parser.add_argument('--feature-extraction-cnn', type=str, default='vgg',
+                    help='Feature extraction architecture: vgg/resnet101')
+# Synthetic dataset parameters
+parser.add_argument('--random-sample', type=str_to_bool, nargs='?', const=True, default=False,
+                    help='sample random transformations')
+
+args = parser.parse_args()
+
+# Seed
+tf.set_random_seed(args.seed)
+
+# Download dataset if needed and set paths
+if args.training_dataset == 'pascal':
+    if args.training_image_path == '':
+        download_pascal('datasets/pascal-voc11/')
+        args.training_image_path = 'datasets/pascal-voc11/'
+    if args.training_tnf_csv == '' and args.geometric_model == 'affine':
+        args.training_tnf_csv = 'training_data/pascal-synth-aff'
+    elif args.training_tnf_csv == '' and args.geometric_model == 'tps':
+        args.training_tnf_csv = 'training_data/pascal-synth-tps'
+
+# CNN model and loss
+print('Creating CNN model...')
+
+model = CNNGeometric(geometric_model=args.geometric_model,
+                     feature_extraction_cnn=args.feature_extraction_cnn)
+
+if args.use_mse_loss:
+    print('Using MSE loss...')
+    loss = tf.losses
+else:
+    print('Using grid loss...')
+    loss = TransformedGridLoss(geometric_model=args.geometric_model)
+
+# Dataset and dataloader
+dataset = SynthDataset(geometric_model=args.geometric_model,
+                       csv_file=os.path.join(args.training_tnf_csv, 'train.csv'),
+                       training_image_path=args.training_image_path,
+                       transform=NormalizeImageDict(['image']),
+                       random_sample=args.random_sample)
+
+"""
+dataloader = DataLoader(dataset, batch_size=args.batch_size,
+                        shuffle=True, num_workers=4) """
+
+dataset_test = SynthDataset(geometric_model=args.geometric_model,
+                            csv_file=os.path.join(args.training_tnf_csv, 'test.csv'),
+                            training_image_path=args.training_image_path,
+                            transform=NormalizeImageDict(['image']),
+                            random_sample=args.random_sample)
+
+"""
+dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size,
+                             shuffle=True, num_workers=4) """
+
+pair_generation_tnf = SynthPairTnf(geometric_model=args.geometric_model)
+
+# Optimizer
+cost = loss()
+optimizer = tf.train.AdamOptimizer(learning_rate=args.lr).minimize(loss)#(model.FeatureRegression.parameters(), lr=args.lr)
+
+# Train
+if args.use_mse_loss:
+    checkpoint_name = os.path.join(args.trained_models_dir,
+                                   args.trained_models_fn + '_' + args.geometric_model + '_mse_loss' + args.feature_extraction_cnn + '.pth.tar')
+else:
+    checkpoint_name = os.path.join(args.trained_models_dir,
+                                   args.trained_models_fn + '_' + args.geometric_model + '_grid_loss' + args.feature_extraction_cnn + '.pth.tar')
+
+best_test_loss = float("inf")
+
+print('Starting training...')
+
+# Iteration section
+for epoch in range(1, args.num_epochs + 1):
+    train_loss = train(epoch, model, loss, optimizer, dataloader, pair_generation_tnf, log_interval=100)
+    test_loss = test(model, loss, dataloader_test, pair_generation_tnf)
+
+    # remember best loss
+    is_best = test_loss < best_test_loss
+    best_test_loss = min(test_loss, best_test_loss)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'args': args,
+        'state_dict': model.state_dict(),
+        'best_test_loss': best_test_loss,
+        'optimizer': optimizer.state_dict(),
+    }, is_best, checkpoint_name)
+
+print('Done!')
